6a7ce42987d3028d281e3f1a7fbb2f814c477b0c
shet6006 <shet6006@gmail.com>
2025-12-27 22:32:02 +0900

    회원가입, 회원조회 기능 생성

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,13 +1,16 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
+
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
